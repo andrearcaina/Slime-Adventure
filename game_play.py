e57f420ccd9a738c7f6086602c490f341158ca4c
Tristanv0 <cheng.tristan@hotmail.com>
2023-01-23 18:29:25 -0500

    Update game_play.py with game states
    -When N = 0, nothing is happening to the game
    -When R = 1, the game is running
    -When E = 2, the game is ended

--- game_play.py
+++ game_play.py
@@ -1,3 +1,6 @@
 from enum import Enum
 
-#class GameState(Enum):
+class GameState(Enum):
+    N = 0   #N is nothing is happening 
+    R = 1   #R is game is running 
+    E = 2   #E is game is ending
